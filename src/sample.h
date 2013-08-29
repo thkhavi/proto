b480d4b346be456d7e60ab94784ed9b6d202eaab
Frogee <ryanabashbash@tamu.edu>
2013-08-29 13:20:04 -0500

    On branch rfm_dev  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   sample.h

---
+++ src/sample.h
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sample.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  13-08-29 01:17:32 PM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Ryan F. McCormick, Sandra K. Truong
+ *   Organization:  Texas A&M University
+ *
+ * =====================================================================================
+ */
+

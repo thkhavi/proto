044bc692419d34ca441cfaeac9a4458d31c1de39
PrecociousLlama <thkhavi@neo.tamu.edu>
2013-09-05 08:11:42 -0500

    Update stacksDB.cpp

--- src/stacksDB.cpp
+++ src/stacksDB.cpp
@@ -61,3 +61,45 @@ void getSamples(const char *pcc_db, const char *pcc_server,
 		}
 	}
 }
+
+
+void getTags(const char *pcc_db, const char *pcc_server,
+	       const char *pcc_user, const char *pcc_password, 
+	       std::vector<Tags> * pvTags_tags)
+{
+	std::cerr << "Hello from getTags" << std::endl;
+	//Iterators
+	int i, j;
+	
+	int depthSum;
+	int depthAvg;
+
+	// Connect to the sample database.
+	mysqlpp::Connection conn(false);
+	if (conn.connect(pcc_db, pcc_server, pcc_user, pcc_password)) {
+		//Execute the query for the samples, and process the return
+		mysqlpp::Query tagQuery = conn.query("SELECT tag_id, chr, bp, strand FROM catalog_tags");
+
+		if (mysqlpp::StoreQueryResult tagRes = tagQuery.store()) {
+			for (i = 0; i < tagRes.num_rows(); i++) {
+				Tag tag;
+				tag.setName(tagRes[i]["file"]);
+				tag.setID(sampleRes[i]["sample_id"]);
+				
+				//Retrieve the depth for every tag contained within the sample and calculate average
+				depthSum = 0;
+				mysqlpp::Query depthQuery = conn.query("SELECT depth FROM tag_index \
+						WHERE sample_id=%0:idval");
+				depthQuery.parse();
+				if (mysqlpp::StoreQueryResult depthRes = depthQuery.store(sampleRes[i]["sample_id"])) {
+						for (j = 0; j < depthRes.num_rows(); j++) {
+							depthSum = depthSum + depthRes[j]["depth"];
+						}
+						depthAvg = depthSum / (depthRes.num_rows());
+						sample.setAvgDepth(depthAvg);
+				}
+				pvSamples_samples->push_back(sample);
+			}
+		}
+	}
+}
